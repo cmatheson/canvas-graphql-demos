de86bdabe8cd43d7741108618bee913cff7d2dfb
Cameron Matheson <cameron.matheson@gmail.com>
2019-02-04 14:28:06 -0700

    check proptypes on SimpleQueryExample

--- src/SimpleQueryExample.js
+++ src/SimpleQueryExample.js
@@ -1,54 +1,64 @@
 import React from "react";
 import { Query, gql } from "./canvas-apollo";
+import PropTypes from 'prop-types'
+import { propType } from "graphql-anywhere";
 
-let SimpleQueryExample = ({ courseId }) => (
-  <Query
-    query={gql`
-      query CourseOverviewQuery($courseId: ID!) {
-        course(id: $courseId) {
-          id
-          name
-          state
-          sectionsConnection {
-            edges {
-              node {
-                id
-                name
-              }
-            }
+let QUERY = gql`
+  query CourseOverviewQuery($courseId: ID!) {
+    course(id: $courseId) {
+      id
+      name
+      state
+      sectionsConnection {
+        edges {
+          node {
+            id
+            name
           }
         }
       }
-    `}
-    variables={{ courseId }}
-  >
-    {({ data, loading, error }) => {
-      if (error) {
-        return <strong>Something went wrong</strong>;
-      }
-      if (loading) {
-        return "...";
-      }
-      let course = data.course;
+    }
+  }
+`;
 
-      if (!course) return "Not found";
-
-      return (
-        <div>
-          <h2>{course.name}</h2>
-          <p>
-            State: <strong>{course.state}</strong>
-          </p>
-          <div>Sections:</div>
-          <ul>
-            {course.sectionsConnection.edges.map(edge => (
-              <li key={edge.node.id}>{edge.node.name}</li>
-            ))}
-          </ul>
-        </div>
-      );
-    }}
+let SimpleQueryExample = ({ courseId }) => (
+  <Query query={QUERY} variables={{ courseId }}>
+    {props => <CourseOverview {...props} />}
   </Query>
 );
+
+class CourseOverview extends React.Component {
+  render() {
+    const { data, loading, error } = this.props;
+
+    if (error) {
+      return <strong>Something went wrong</strong>;
+    }
+    if (loading) {
+      return "...";
+    }
+    let course = data.course;
+
+    if (!course) return "Not found";
+
+    return (
+      <div>
+        <h2>{course.name}</h2>
+        <p>
+          State: <strong>{course.state}</strong>
+        </p>
+        <div>Sections:</div>
+        <ul>
+          {course.sectionsConnection.edges.map(edge => (
+            <li key={edge.node.id}>{edge.node.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+CourseOverview.propTypes = {
+  course: propType(QUERY)
+}
 
 export default SimpleQueryExample;
